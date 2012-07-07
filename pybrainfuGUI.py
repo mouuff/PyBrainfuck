007ce3cba4f460f5d20fadc41ff6971ea79fecef
Arnaud Aliès <arnaudalies.py@gmail.com>
2012-07-07 07:31:11 +0300

    Update master

--- pybrainfuGUI.py
+++ pybrainfuGUI.py
@@ -3,100 +3,183 @@
 
 import wx
 
-def BF(script):
-	''''Launch a Brainfuck script'''
-	table = [0]*3000
-	pointer = 0
-	progress = 0
-	out = ''
-	loop = 0
-	while (progress < len(script)):
-		if (script[progress] in '+-<>.,[]'):
-			if (script[progress] == '+'):
-				table[pointer] += 1
+__all__ = ('pybf')
+__doc__ = ('''
+PyBF
+A tiny and bugged brainfuck interpreter
+Classes:
+	pybf
+		load(script):
+			load a script
+		add(part)
+			add a part of the script to the script
+		clean()
+			clean up the code, useful on big scripts
+		check()
+			do a syntax check, returns a SyntaxError if the script is wrong
+		execute()
+			execute the script, use load before.
+			you can use # to set ad break point to debug your code.
+''')
+
+class pybf:
+	def __init__(self):
+		'''Brainfuck interpreter'''
+		self.table = [0]*30000
+		self.pointer = 0
+		self.progress = 0
+		self.loop = 0
+		self.script = ''
+
+	
+	def load(self, script):
+		'''Load a script, this is requiered'''
+		self.script = script
+	
+	def add(self, part):
+		'''Add code to the script'''
+		self.script += part
+	
+	def clean(self):
+		'''option to clean script to speed up big scripts'''
+		script = ''
+		removed = 0
+		for char in self.script:
+			if (char in '+-<>.,[]#'):
+				script += char
+			else:
+				removed += 1
+		self.script = script
+		return removed
+
+	def check(self):
+		'''Check if any errors, not complete yet.'''
+		if (self.script.count("[") != self.script.count("]")):
+			raise SyntaxError
+		if (self.script.find("[]") != -1):
+			raise SyntaxError
+		
+			
+	def execute(self, xinput=""):
+		out= 'Output:\n'
+		placement = 0
+		while (self.progress < len(self.script)):
+			if (self.script[self.progress] in '+-<>.,[]#'):
+				if (self.script[self.progress] == '+'):
+					self.table[self.pointer] += 1
+					
+				elif (self.script[self.progress] == '-'):
+					if (self.table[self.pointer] > 0):
+						self.table[self.pointer] -= 1
+					
+				elif (self.script[self.progress] == '>'):
+					self.pointer += 1
+					
+				elif (self.script[self.progress] == '<'):
+					if (self.pointer > 0):
+						self.pointer -= 1
+					
+				elif (self.script[self.progress] == '.'):
+					try:
+						out += chr(self.table[self.pointer])
+					except (ValueError):
+						out += '?'
+					
+				elif (self.script[self.progress] == ','):
+					if (placement < len(xinput)):
+						self.table[self.pointer] = ord(xinput[placement])
+						placement += 1
+
+					
+				elif (self.script[self.progress] == '['):
+					self.loop += 1
 				
-			elif (script[progress] == '-'):
-				table[pointer] -= 1
+				elif (self.script[self.progress] == '#'):
+					out +=  str(list(set(self.table)))
 				
-			elif (script[progress] == '>'):
-				pointer += 1
-				
-			elif (script[progress] == '<'):
-				pointer -= 1
-				
-			elif (script[progress] == '.'):
-				try:
-					out += (chr(table[pointer]))
-				except (ValueError):
-					out += '?'
-				
-			elif (script[progress] == ','):
-				table[pointer] = ord(raw_input())
-				
-			elif (script[progress] == '['):
-				loop += 1
-				
-			elif (loop):
-				if (script[progress] == ']'):
-					if table[pointer] > 0:
-						progress = script.find('[')
-					else:
-						loop -= 1
-						if (loop <= 0):
-							progress = 0
-							script = script[script.find("]")+1:]
-		progress += 1
-	print list(set(table))
-	return out
+				elif (self.loop):
+					if (self.script[self.progress] == ']'):
+						if self.table[self.pointer] > 0:
+							self.progress = self.script.find('[')
+						else:
+							self.loop -= 1
+							if (not self.loop):
+								self.progress = 0
+								self.script = self.script[self.script.find("]")+1:]
+			self.progress += 1
+		return out
 
 
 class MyFrame(wx.Frame):
-    def __init__(self, *args, **kwds):
-        kwds["style"] = wx.DEFAULT_FRAME_STYLE
-        wx.Frame.__init__(self, *args, **kwds)
-        self.label_2 = wx.StaticText(self, -1, "PyBrainFuu")
-        self.label_3 = wx.StaticText(self, -1, "Enter some BF code:")
-        self.xinput = wx.TextCtrl(self, -1, "")
-        self.button_1 = wx.Button(self, -1, "OK")
-        self.output = wx.StaticText(self, -1, "")
+	def __init__(self, *args, **kwds):
+		# begin wxGlade: MyFrame.__init__
+		kwds["style"] = wx.DEFAULT_FRAME_STYLE
+		wx.Frame.__init__(self, *args, **kwds)
+		self.label_1 = wx.StaticText(self, -1, "Enter code below:")
+		self.label_2 = wx.StaticText(self, -1, "Script:")
+		self.script = wx.TextCtrl(self, -1, "")
+		self.label_3 = wx.StaticText(self, -1, "Pre-input:")
+		self.preinput = wx.TextCtrl(self, -1, "")
+		self.output = wx.StaticText(self, -1, "Output: ")
+		self.compile = wx.Button(self, -1, "OK")
 
-        self.__set_properties()
-        self.__do_layout()
+		self.__set_properties()
+		self.__do_layout()
 
-        self.Bind(wx.EVT_TEXT, self.oninput, self.xinput)
-        self.Bind(wx.EVT_BUTTON, self.out, self.button_1)
+		self.Bind(wx.EVT_TEXT_ENTER, self.xscript, self.script)
+		self.Bind(wx.EVT_TEXT_ENTER, self.xpreinput, self.preinput)
+		self.Bind(wx.EVT_BUTTON, self.start, self.compile)
+		# end wxGlade
 
-    def __set_properties(self):
-        self.SetTitle("Brainfuck interpreter")
+	def __set_properties(self):
+		# begin wxGlade: MyFrame.__set_properties
+		self.SetTitle("Pybrainfuck")
+		# end wxGlade
 
-    def __do_layout(self):
-        sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        grid_sizer_1 = wx.GridSizer(2, 2, 0, 0)
-        grid_sizer_2 = wx.GridSizer(1, 2, 0, 0)
-        grid_sizer_2.Add(self.label_2, 0, wx.EXPAND|wx.ADJUST_MINSIZE, 0)
-        grid_sizer_2.Add(self.label_3, 0, wx.EXPAND|wx.ADJUST_MINSIZE, 0)
-        grid_sizer_1.Add(grid_sizer_2, 1, wx.EXPAND, 0)
-        grid_sizer_1.Add(self.xinput, 0, wx.EXPAND|wx.ADJUST_MINSIZE, 0)
-        grid_sizer_1.Add(self.button_1, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL|wx.ADJUST_MINSIZE, 0)
-        grid_sizer_1.Add(self.output, 0, wx.EXPAND|wx.ADJUST_MINSIZE, 0)
-        sizer_1.Add(grid_sizer_1, 1, wx.EXPAND, 0)
-        self.SetSizer(sizer_1)
-        sizer_1.Fit(self)
-        self.Layout()
+	def __do_layout(self):
+		# begin wxGlade: MyFrame.__do_layout
+		sizer_1 = wx.BoxSizer(wx.VERTICAL)
+		grid_sizer_1 = wx.GridSizer(1, 2, 0, 0)
+		grid_sizer_5 = wx.GridSizer(2, 1, 0, 0)
+		grid_sizer_2 = wx.GridSizer(3, 1, 0, 0)
+		grid_sizer_4 = wx.GridSizer(2, 1, 0, 0)
+		grid_sizer_3 = wx.GridSizer(2, 1, 0, 0)
+		grid_sizer_2.Add(self.label_1, 0, wx.EXPAND, 0)
+		grid_sizer_3.Add(self.label_2, 0, wx.EXPAND, 0)
+		grid_sizer_3.Add(self.script, 0, wx.EXPAND, 0)
+		grid_sizer_2.Add(grid_sizer_3, 1, wx.EXPAND, 0)
+		grid_sizer_4.Add(self.label_3, 0, wx.EXPAND, 0)
+		grid_sizer_4.Add(self.preinput, 0, wx.EXPAND, 0)
+		grid_sizer_2.Add(grid_sizer_4, 1, wx.EXPAND, 0)
+		grid_sizer_1.Add(grid_sizer_2, 1, wx.EXPAND, 0)
+		grid_sizer_5.Add(self.output, 0, 0, 0)
+		grid_sizer_5.Add(self.compile, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 0)
+		grid_sizer_1.Add(grid_sizer_5, 1, wx.EXPAND, 0)
+		sizer_1.Add(grid_sizer_1, 1, wx.EXPAND, 0)
+		self.SetSizer(sizer_1)
+		sizer_1.Fit(self)
+		self.Layout()
+		# end wxGlade
 
-    def oninput(self, event):
-        event.Skip()
+	def xscript(self, event):
+		event.Skip()
 
-    def out(self, event):
-        self.output.SetLabel(BF(self.xinput.GetValue()))
-        event.Skip()
+	def xpreinput(self, event):
+		event.Skip()
 
+	def start(self, event): # wxGlade: MyFrame.<event_handler>
+		compiler = pybf()
+		compiler.load(self.script.GetValue())
+		self.output.SetLabel(compiler.execute(self.preinput.GetValue()))
+		event.Skip()
+
+# end of class MyFrame
 
 
 if __name__ == "__main__":
-    app = wx.PySimpleApp(0)
-    wx.InitAllImageHandlers()
-    frame_1 = MyFrame(None, -1, "")
-    app.SetTopWindow(frame_1)
-    frame_1.Show()
-    app.MainLoop()
+	app = wx.PySimpleApp(0)
+	wx.InitAllImageHandlers()
+	frame_1 = MyFrame(None, -1, "")
+	app.SetTopWindow(frame_1)
+	frame_1.Show()
+	app.MainLoop()
